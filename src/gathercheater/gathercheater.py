864f9643b987c5914ace24e7b170b7b7c47a1a07
Mat Y <me@matyopp.com>
2022-11-24 18:36:58 -0800

    variable updates & doc updates

--- src/gathercheater/gathercheater.py
+++ src/gathercheater/gathercheater.py
@@ -6,59 +6,64 @@ import gathercheater.constants as c
 class GatherCheater:
 
     def __init__(self):
-        self.lichess = lichess_access()
-        self.__user = c.USER.lower()
-        self.__start = c.START
-        self.__end = c.END
-        self.__max_games = c.MAX_GAMES
+        self.__lichess = lichess_access()
+        self._user = c.USER.lower()
+        self._start = c.START
+        self._end = c.END
+        self._max_games = c.MAX_GAMES
         self.df_index = 0
         self.__api_limit = c.LICHESS_API_LIMIT
         self.data_list = []
 
     @property
     def user(self):
-        return self.__user
+        """Return the currently set user"""
+        return self._user
 
     @user.setter
     def user(self, value: str):
-        """Set user to check games"""
-        self.__user = value.lower()
+        """Set user for games to check"""
+        self._user = value.lower()
 
     @property
     def start(self):
-        return self.__start
+        """Return the set Start Datetime"""
+        return self._start
 
     @start.setter
     def start(self, value: str):
-        """Use YYYY/m/d format"""
+        """Set the start date using YYYY/m/d format"""
         value_format = '%Y/%m/%d'
         try:
-            self.__start = dt.datetime.strptime(value, value_format)
+            self._start = dt.datetime.strptime(value, value_format)
 
-        except (Exception,):
+        except (ValueError,):
             raise ValueError('Format is probably invalid')
 
     @property
     def end(self):
-        return self.__end
+        """Return the set end date for games to check"""
+        return self._end
 
     @end.setter
     def end(self, value: str):
         """Use yyyy/m/d format to set datetime object"""
         value_format = '%Y/%m/%d'
         try:
-            self.__end = dt.datetime.strptime(value, value_format)
-        except (Exception,):
+            self._end = dt.datetime.strptime(value, value_format)
+        except (ValueError,):
             raise ValueError('Format is probably invalid')
 
     @property
     def max_games(self):
-        return self.__max_games
+        """Return the set number of max games to review"""
+        return self._max_games
 
     @max_games.setter
     def max_games(self, value: int):
+        """Set the total number of games to review"""
         if value > 0:
-            self.__max_games = value
+            self._max_games = value
         else:
             raise ValueError('Value cannot be <= 0')
 
@@ -67,23 +72,24 @@ class GatherCheater:
         return self.__api_limit
 
     def games_by_player_dates(self, berserk_start, berserk_end):
-        """Searches Lichess API for games from a user between 2 dates & returns those games as a generator"""
+        """Search Lichess API for games from a user between 2 dates & returns those games as a generator"""
 
         starting = berserk_start
         ending = berserk_end
 
-        games_data = self.lichess.games.export_by_player(self.__user, since=starting, until=ending,
-                                                         max=self.__max_games)
+        games_data = self.__lichess.games.export_by_player(self._user, since=starting, until=ending,
+                                                           max=self._max_games)
 
         return games_data
 
     def games_by_player_list(self, players):
-        api_data = self.lichess.users.get_by_id(players)
+        """Get game data by a string of player names"""
+        api_data = self.__lichess.users.get_by_id(players)
         return api_data
 
     @staticmethod
     def get_players_from_games(game_list):
-        """get list of players from the list of dicts, returned from games_by_player_dates"""
+        """get list of player usernames from the list of dicts returned from the games_by_player_list function"""
 
         p_list = []
         for game in game_list:
@@ -95,6 +101,7 @@ class GatherCheater:
 
     @staticmethod
     def check_cheaters(players):
+        """Returns 3 list: violated ToS accounts, Closed Accounts, and accounts with no infractions"""
         cheater_list = []
         closed_accounts = []
         not_cheater = []
@@ -115,6 +122,7 @@ class GatherCheater:
 
     @staticmethod
     def display_data(players):  # pragma: no cover
+        """Displays the 3 lists populated from the check_cheaters function"""
         (violated, closed, good) = GatherCheater.check_cheaters(players)
         (v_total, c_total, g_total) = [len(violated), len(closed), len(good)]
 
