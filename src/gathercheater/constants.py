a1d49231c69568393ef54c10decbd59da69f8523
Mat Y <me@matyopp.com>
2023-06-17 14:06:39 -0700

    updated end date constant for 2023

--- src/gathercheater/constants.py
+++ src/gathercheater/constants.py
@@ -3,8 +3,8 @@ import datetime as dt
 USER = 'basilcandle'
 DF_INDEX = 0
 START = dt.datetime(2022, 1, 1)
-END = dt.datetime(2022, 12, 31)
-MAX_GAMES = 50
+END = dt.datetime(2023, 12, 31)
+MAX_GAMES = 100
 
 """Do Not Modify"""
 LICHESS_API_LIMIT = 300  # do not change this.
